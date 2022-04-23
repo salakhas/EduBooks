8a8f8182d2f70512f0ed53efc19028b59b3c8443
pankajbisht03 <pankajbishtuk04@gmail.com>
2022-04-23 23:54:32 +0530

    autheticated

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,7 +3,62 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import GoogleIcon from '@mui/icons-material/Google';
 
+import {useEffect} from "react";
+import { useNavigate } from 'react-router-dom';
+import { useAuthState } from "react-firebase-hooks/auth";
+
+
+// Firebase
+import { app } from './../../firebase.config';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup
+} from 'firebase/auth';
+
+
+
+
+//
+
 export const Login = () => {
+    //
+    const auth = getAuth();
+  const [user, loading, error] = useAuthState(auth);
+  let navigate = useNavigate();
+
+   
+  const provider = new GoogleAuthProvider();
+
+  const addData = () => {
+    signInWithPopup(auth, provider)
+    
+    //signInWithEmailAndPassword(auth, data.email, data.password)
+  }
+  const signout =()=>{
+    auth.signOut()
+    alert("singout")
+    // navigate("/dd")
+    
+  }
+
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    if (user){
+        console.log(user.displayName)
+        auth.signOut()
+        navigate("/");}
+  
+  }, [user, loading]);
+
+
+  //
     return (
         <div className="divLoginDiv">
             <div className="register">
@@ -23,7 +78,8 @@ export const Login = () => {
                     <TextField id="outlined-basic" label="Password" variant="outlined" />
                     <Button variant="contained" style={{backgroundColor:"#12D7B9",color:"white"}}>Log In</Button>
                     <span style={{fontSize:"20px",color:"grey"}}>- OR -</span>
-                    <Button variant="contained" style={{backgroundColor:"#12D7B9",color:"white"}}><span><GoogleIcon/>&nbsp;&nbsp;</span>Sign in with Google</Button>
+                    <Button onClick = {addData} variant="contained" style={{backgroundColor:"#12D7B9",color:"white"}}><span><GoogleIcon/>&nbsp;&nbsp;</span>Sign in with Google</Button>
+                    <Button onClick = {signout} variant="contained" style={{backgroundColor:"#12D7B9",color:"white"}}><span><GoogleIcon/>&nbsp;&nbsp;</span>Sign out</Button>
                 </div>
             </div>
         </div>
